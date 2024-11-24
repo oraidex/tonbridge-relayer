71e04a26b5ea298e31df88ea843712d8b295e3bf
ducphamle2 <ducphamle212@gmail.com>
2024-11-23 17:30:55 -0800

    fix: throw error when offset hash undefined

--- packages/ton-to-cw/src/tx-processor.ts
+++ packages/ton-to-cw/src/tx-processor.ts
@@ -90,10 +90,9 @@ export default class TonTxProcessor {
       if (indexOf === -1) {
         const oldestTx = txs[txs.length - 1].tx;
         if (!oldestTx.prevTransactionHash) {
-          this.logger.error(
+          throw new Error(
             "TonTxProcessor queryUnprocessedTransactions new offset hash is undefined"
           );
-          continue;
         }
         transactions.push(...txs);
         // increase offset and continue querying txs until we find our oldest transaction that we can remember
